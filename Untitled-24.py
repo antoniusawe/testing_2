3bd78c6e4a7100d720dfb404b640298df40c9d4c
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-09-27 09:26:12 +0700

    Update Untitled-24.py

--- Untitled-24.py
+++ Untitled-24.py
@@ -11,7 +11,19 @@ import streamlit as st
 # In[9]:
 
 
-df = pd.read_csv('https://github.com/antoniusawe/testing_2/blob/main/data%20karyawan%20aktif%20per%2026%20September%202024.csv', encoding='ISO-8859-1')
+import pandas as pd
+import streamlit as st
+
+# Membaca file CSV dari tautan raw GitHub
+url = 'https://raw.githubusercontent.com/antoniusawe/testing_2/main/data%20karyawan%20aktif%20per%2026%20September%202024.csv'
+df = pd.read_csv(url, encoding='ISO-8859-1')
+
+# Menampilkan judul di aplikasi Streamlit
+st.title("Data Karyawan Aktif per 26 September 2024")
+
+# Menampilkan data dalam tabel menggunakan Streamlit
+st.write(df)
+
 
 
 # In[10]:
