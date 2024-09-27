c8e4c5e980d3bde99d4d9b879304675a9fb83eb2
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-09-27 09:31:33 +0700

    Update Untitled-24.py

--- Untitled-24.py
+++ Untitled-24.py
@@ -12,7 +12,7 @@ import streamlit as st
 
 # Membaca file CSV dari tautan raw GitHub
 url = 'https://github.com/antoniusawe/testing_2/blob/main/data%20karyawan%20aktif%20per%2026%20September%202024.csv'
-df = pd.read_csv(url, encoding='ISO-8859-1')
+df = pd.read_csv(url, encoding='ISO-8859-1', on_bad_lines='skip')
 
 # Menampilkan judul di aplikasi Streamlit
 st.title("Data Karyawan Aktif per 26 September 2024")
